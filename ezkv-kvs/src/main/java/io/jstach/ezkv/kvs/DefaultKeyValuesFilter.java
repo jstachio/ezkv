e501e9f08a0b7d825fd3e1279f491fb4980e55cb
Adam Gent <adam.gent@snaphop.com>
2024-12-29 10:55:16 -0500

    Add key or value target to filter

--- ezkv-kvs/src/main/java/io/jstach/ezkv/kvs/DefaultKeyValuesFilter.java
+++ ezkv-kvs/src/main/java/io/jstach/ezkv/kvs/DefaultKeyValuesFilter.java
@@ -13,24 +13,29 @@ enum DefaultKeyValuesFilter implements KeyValuesFilter {
 	GREP(KeyValuesResource.FILTER_GREP) {
 
 		@Override
-		protected KeyValues doFilter(FilterContext context, KeyValues keyValues, String expression) {
+		protected KeyValues doFilter(FilterContext context, KeyValues keyValues, String expression, Target target) {
 			String grep = expression;
 			// TODO error handling.
 			Objects.requireNonNull(keyValues);
 			Pattern pattern = Pattern.compile(grep);
 			return keyValues.filter(kv -> {
-				return pattern.matcher(kv.key()).find();
+				String v = switch(target) {
+				case KEY -> kv.key();
+				case VALUE -> kv.value();
+				};
+				return pattern.matcher(v).find();
 			});
 		}
 	},
 	SED(KeyValuesResource.FILTER_SED) {
 
 		@Override
-		protected KeyValues doFilter(FilterContext context, KeyValues keyValues, String expression) {
+		protected KeyValues doFilter(FilterContext context, KeyValues keyValues, String expression, Target target) {
 			String sed = expression;
 			Objects.requireNonNull(sed);
 			var command = DefaultSedParser.parse(sed);
-			return keyValues.flatMap(kv -> {
+			return switch(target) {
+			case KEY ->  keyValues.flatMap(kv -> {
 				String key = command.execute(kv.key());
 				if (key == null) {
 					return KeyValues.empty();
@@ -40,12 +45,24 @@ enum DefaultKeyValuesFilter implements KeyValuesFilter {
 				}
 				return KeyValues.of(kv.withKey(key));
 			});
+			case VALUE ->  keyValues.flatMap(kv -> {
+				String value = command.execute(kv.value());
+				if (value == null) {
+					return KeyValues.empty();
+				}
+				if (kv.value().equals(value)) {
+					return KeyValues.of(kv);
+				}
+				return KeyValues.of(kv.withExpanded(value));
+			});
+			};
+
 		}
 
 	},
 	JOIN(KeyValuesResource.FILTER_JOIN) {
 		@Override
-		protected KeyValues doFilter(FilterContext context, KeyValues keyValues, String expression) {
+		protected KeyValues doFilter(FilterContext context, KeyValues keyValues, String expression, Target target) {
 			Objects.requireNonNull(keyValues);
 			SequencedMap<String, KeyValue> m = new LinkedHashMap<>();
 			for (var kv : keyValues) {
@@ -72,12 +89,42 @@ enum DefaultKeyValuesFilter implements KeyValuesFilter {
 	@Override
 	public Optional<KeyValues> filter(FilterContext context, KeyValues keyValues, Filter filter) {
 		String filterName = filter.filter();
-		if (this.filter.equalsIgnoreCase(filterName)) {
-			return Optional.of(doFilter(context, keyValues, filter.expression()));
+		Target target;
+		String resolvedFilterName;
+//		String targetString = context.parameters().getValue("filter_target");
+//		if (targetString != null) {
+//			target = switch(targetString) {
+//			case "key" -> Target.KEY;
+//			case "value" ->Target.VALUE;
+//			default -> 
+//				throw new IllegalArgumentException("filter_target is invalid and should be 'key' or 'value'. got '" + targetString + "'");
+//			};
+//			resolvedFilterName = filterName;
+//		}
+		if (filterName.endsWith("_key")) {
+			target = Target.KEY;
+			resolvedFilterName = filterName.substring(0, filterName.length() - "_key".length());
+		}
+		else if (filterName.endsWith("_value")) {
+			target = Target.VALUE;
+			resolvedFilterName = filterName.substring(0, filterName.length() - "_value".length());
+		}
+		else {
+			target = Target.KEY;
+			resolvedFilterName = filterName;
+		}
+		
+		if (this.filter.equalsIgnoreCase(resolvedFilterName)) {
+			return Optional.of(doFilter(context, keyValues, filter.expression(), target));
 		}
 		return Optional.empty();
 	}
 
-	protected abstract KeyValues doFilter(FilterContext context, KeyValues keyValues, String expression);
+	protected abstract KeyValues doFilter(FilterContext context, KeyValues keyValues, String expression, Target target);
+	
+	enum Target {
+		KEY,
+		VALUE;
+	}
 
 }
