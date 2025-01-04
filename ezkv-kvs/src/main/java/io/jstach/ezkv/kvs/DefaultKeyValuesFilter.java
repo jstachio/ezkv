4cb7f1e42c8c53cfe3b721799b33d62259cc4f99
Adam Gent <adam.gent@snaphop.com>
2025-01-03 23:12:33 -0500

    Add documentation filters target

--- ezkv-kvs/src/main/java/io/jstach/ezkv/kvs/DefaultKeyValuesFilter.java
+++ ezkv-kvs/src/main/java/io/jstach/ezkv/kvs/DefaultKeyValuesFilter.java
@@ -19,9 +19,9 @@ enum DefaultKeyValuesFilter implements KeyValuesFilter {
 			Objects.requireNonNull(keyValues);
 			Pattern pattern = Pattern.compile(grep);
 			return keyValues.filter(kv -> {
-				String v = switch(target) {
-				case KEY -> kv.key();
-				case VALUE -> kv.value();
+				String v = switch (target) {
+					case KEY -> kv.key();
+					case VALUE -> kv.value();
 				};
 				return pattern.matcher(v).find();
 			});
@@ -34,27 +34,27 @@ enum DefaultKeyValuesFilter implements KeyValuesFilter {
 			String sed = expression;
 			Objects.requireNonNull(sed);
 			var command = DefaultSedParser.parse(sed);
-			return switch(target) {
-			case KEY ->  keyValues.flatMap(kv -> {
-				String key = command.execute(kv.key());
-				if (key == null) {
-					return KeyValues.empty();
-				}
-				if (kv.key().equals(key)) {
-					return KeyValues.of(kv);
-				}
-				return KeyValues.of(kv.withKey(key));
-			});
-			case VALUE ->  keyValues.flatMap(kv -> {
-				String value = command.execute(kv.value());
-				if (value == null) {
-					return KeyValues.empty();
-				}
-				if (kv.value().equals(value)) {
-					return KeyValues.of(kv);
-				}
-				return KeyValues.of(kv.withExpanded(value));
-			});
+			return switch (target) {
+				case KEY -> keyValues.flatMap(kv -> {
+					String key = command.execute(kv.key());
+					if (key == null) {
+						return KeyValues.empty();
+					}
+					if (kv.key().equals(key)) {
+						return KeyValues.of(kv);
+					}
+					return KeyValues.of(kv.withKey(key));
+				});
+				case VALUE -> keyValues.flatMap(kv -> {
+					String value = command.execute(kv.value());
+					if (value == null) {
+						return KeyValues.empty();
+					}
+					if (kv.value().equals(value)) {
+						return KeyValues.of(kv);
+					}
+					return KeyValues.of(kv.withExpanded(value));
+				});
 			};
 
 		}
@@ -91,29 +91,27 @@ enum DefaultKeyValuesFilter implements KeyValuesFilter {
 		String filterName = filter.filter();
 		Target target;
 		String resolvedFilterName;
-//		String targetString = context.parameters().getValue("filter_target");
-//		if (targetString != null) {
-//			target = switch(targetString) {
-//			case "key" -> Target.KEY;
-//			case "value" ->Target.VALUE;
-//			default -> 
-//				throw new IllegalArgumentException("filter_target is invalid and should be 'key' or 'value'. got '" + targetString + "'");
-//			};
-//			resolvedFilterName = filterName;
-//		}
-		if (filterName.endsWith("_key")) {
+
+		if (filterName.endsWith(KeyValuesResource.FILTER_TARGET_KEY)) {
 			target = Target.KEY;
-			resolvedFilterName = filterName.substring(0, filterName.length() - "_key".length());
+			resolvedFilterName = filterName.substring(0,
+					filterName.length() - KeyValuesResource.FILTER_TARGET_KEY.length());
 		}
-		else if (filterName.endsWith("_value")) {
+		else if (filterName.endsWith(KeyValuesResource.FILTER_TARGET_VAL)) {
 			target = Target.VALUE;
-			resolvedFilterName = filterName.substring(0, filterName.length() - "_value".length());
+			resolvedFilterName = filterName.substring(0,
+					filterName.length() - KeyValuesResource.FILTER_TARGET_VAL.length());
+		}
+		else if (filterName.endsWith(KeyValuesResource.FILTER_TARGET_VALUE)) {
+			target = Target.VALUE;
+			resolvedFilterName = filterName.substring(0,
+					filterName.length() - KeyValuesResource.FILTER_TARGET_VALUE.length());
 		}
 		else {
 			target = Target.KEY;
 			resolvedFilterName = filterName;
 		}
-		
+
 		if (this.filter.equalsIgnoreCase(resolvedFilterName)) {
 			return Optional.of(doFilter(context, keyValues, filter.expression(), target));
 		}
@@ -121,10 +119,11 @@ enum DefaultKeyValuesFilter implements KeyValuesFilter {
 	}
 
 	protected abstract KeyValues doFilter(FilterContext context, KeyValues keyValues, String expression, Target target);
-	
+
 	enum Target {
-		KEY,
-		VALUE;
+
+		KEY, VALUE;
+
 	}
 
 }
